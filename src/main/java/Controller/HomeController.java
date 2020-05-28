4773a74fd46dc02bbb1e10621ab5d309579a761f
RebeccaVillwock <rvillwock1@my.wctc.edu>
2020-05-28 17:41:26 -0500

    add hibernate and spring get combined(added the error page)

--- src/main/java/Controller/HomeController.java
+++ src/main/java/Controller/HomeController.java
@@ -1,7 +1,10 @@
 package Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
@@ -9,5 +12,26 @@ public class HomeController {
     @RequestMapping("/")
     public String showHomePage(){
         return "index";
+    }
+
+    @RequestMapping("/errors")
+    public String handleErrors(HttpServletRequest request,
+                               Model theModel){
+        int httpErrorCode = (Integer)request.getAttribute("javax.servlet.error.status_code");
+
+        String userFriendlyMessage = "Something went wrong!";
+
+        switch(httpErrorCode){
+            case 404:
+                userFriendlyMessage = "404 Page not found please try something else";
+                break;
+            case 500:
+                userFriendlyMessage = "500 something went wrong on our end";
+                break;
+        }
+
+        theModel.addAttribute("errorMessage",userFriendlyMessage);
+
+        return "error";
     }
 }
---
+++ src/main/webapp/WEB-INF/view/error.jsp
@@ -0,0 +1,23 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: becky
+  Date: 5/28/2020
+  Time: 5:40 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Error</title>
+</head>
+<body>
+
+<div id="container">
+    <div id="content">
+        <h3>It seems something has gone wrong</h3>
+        <p>${errorMessage}</p>
+    </div>
+</div>
+
+</body>
+</html>
